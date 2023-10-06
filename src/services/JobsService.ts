045ced81d0ae226c4207f669104bd13fe9027dea
Cristiano Lopes <jirous@gmail.com>
2023-10-06 16:19:06 -0300

    fix fetchJobs Call

--- src/services/JobsService.ts
+++ src/services/JobsService.ts
@@ -2,7 +2,7 @@ import * as FirebaseService from "./FirebaseService";
 
 const getJobsList = async (showEnded: boolean = false) => {
   const fetchJobsResult = await FirebaseService.callFirebaseFunction(
-    "fetchJobs"
+    "fetchJobs", {jobId:0,fetchMedia:false}
   );
 
   return fetchJobsResult;
